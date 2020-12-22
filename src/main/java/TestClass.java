9e0cce075802061ff1a90e529403a0178a0f4797
tri135790 <tranthanhtrivn@gmail.com>
2020-12-22 21:27:25 +0700

    first commit

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class TestClass {
+    public static int findCoprime(int n) {
+        boolean found = false;
+        int a = n-2;
+        while (!found) {
+            for (int i = a; i >= 1; i --) {
+                if (a % i == 0 && n % i ==0) {
+                    if ( i == 1) {
+                        found = true;
+                    } else {
+                        a = a - 1;
+                        break;
+                    }
+                }
+            }
+        }
+        return a;
+
+    }
+    public static void main(String args[] ) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());                // Reading input from STDIN
+        System.out.println(TestClass.findCoprime(n));
+
+    }
+}
+
