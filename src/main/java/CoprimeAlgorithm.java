00004f8b25199b033c0efdec9be45eb34be32fc6
tri135790 <tranthanhtrivn@gmail.com>
2020-12-24 07:27:25 +0700

    Pikachu Algorithm

--- src/main/java/CoprimeAlgorithm.java
+++ src/main/java/CoprimeAlgorithm.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-class TestClass {
+
+class CoprimeAlgorithm {
+
     public static int findCoprime(int n) {
         boolean found = false;
-        int a = n-2;
+        int a = n - 2;
         while (!found) {
-            for (int i = a; i >= 1; i --) {
-                if (a % i == 0 && n % i ==0) {
-                    if ( i == 1) {
+            for (int i = a; i >= 1; i--) {
+                if (a % i == 0 && n % i == 0) {
+                    if (i == 1) {
                         found = true;
                     } else {
                         a = a - 1;
@@ -19,11 +21,12 @@ class TestClass {
         return a;
 
     }
-    public static void main(String args[] ) throws Exception {
+
+    public static void main(String args[]) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());                // Reading input from STDIN
-        System.out.println(TestClass.findCoprime(n));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(CoprimeAlgorithm.findCoprime(n));
 
     }
 }
---
+++ src/main/java/PikachuAndGameOfStringAlgorithm.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PikachuAndGameOfStringAlgorithm {
+    public static int findGap(int i, String s, String t) {
+        int gap = t.charAt(i) - s.charAt(i);
+        if (gap < 0) {
+            gap = gap + 26;
+        }
+        return gap;
+    }
+
+    public static int convertSingleLetter(int i, String s, String t) {
+        int gap = PikachuAndGameOfStringAlgorithm.findGap(i, s, t);
+        int dayof13 = (int) Math.floor(gap / 13);
+        return dayof13 + gap - dayof13 * 13;
+    }
+
+    public static int convertMove(int n, String s, String t) {
+        int day = 0;
+        for (int i = 0; i < n; i++) {
+            day = day + PikachuAndGameOfStringAlgorithm.convertSingleLetter(i, s, t);
+        }
+        return day;
+    }
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        String t = br.readLine();
+        System.out.println(PikachuAndGameOfStringAlgorithm.convertMove(n, s, t));
+
+    }
+}
