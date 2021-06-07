f476224cacd3001f19cc98242ccab8a0662c8d80
Thanh Tri TRAN <trantamtri@r2integrated.com>
2021-06-07 15:03:18 +0700

    missing number algo

---
+++ src/main/java/PracticeAlgorithm/MissingNumber.java
@@ -0,0 +1,33 @@
+package PracticeAlgorithm;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class MissingNumber {
+  public static int missingNumber(int[] nums) {
+    Arrays.sort(nums);
+    for ( int i = 0; i < nums.length; i++) {
+      if( i != nums[i]) {
+        return i;
+      }
+    }
+    return nums.length;
+  }
+
+  public static int missingNumber2(int[] nums) {
+    int sum = 0;
+    int sumMax = 0;
+    for (int i = 0; i < nums.length; i++) {
+      sum = sum + nums[i];
+      sumMax = sumMax + i;
+    }
+    sumMax = sumMax + nums.length;
+    return sumMax - sum;
+  }
+
+  public static void main (String[] args) {
+    int[] nums = {9,6,4,2,3,5,7,0,1};
+    System.out.println(missingNumber(nums));
+    System.out.println(missingNumber2(nums));
+  }
+}
--- src/main/java/PracticeAlgorithm/ValidAnagram.java
+++ src/main/java/PracticeAlgorithm/ValidAnagram.java
@@ -11,7 +11,24 @@ public class ValidAnagram {
     return Arrays.equals(arrs, arrt);
 
   }
-  public static void main (String[] args) {
+
+  public static boolean isAnagram2(String s, String t) {
+    if (s.length() != t.length()) {
+      return  false;
+    }
+    int[] counter = new int[26];
+    for (int i = 0;i < s.length(); i++) {
+      counter[s.charAt(i) - 'a']++;
+      counter[t.charAt(i) - 'a']--;
+    }
+    for (int count : counter) {
+      if (count != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+    public static void main (String[] args) {
     String s = "rac";
     String t = "car";
     System.out.println(isAnagram(s,t));
