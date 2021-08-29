43064f1c7462594c1988a9cebe4a1d6695dbbbbc
Thanh Tri TRAN <tranthanhtrivn@gmail.com>
2021-08-29 11:13:51 +0700

    rotate array one more solution

--- src/main/java/PracticeAlgorithm/RotateArray.java
+++ src/main/java/PracticeAlgorithm/RotateArray.java
@@ -23,9 +23,28 @@ public class RotateArray {
     }
   }
 
+  public static void rotate2(int[] nums, int k) {
+    k  %= nums.length;
+    int shift = nums.length - k;
+
+    int[] store = new int[shift];
+    for (int i = 0; i < store.length; i++) {
+      store[i] = nums[i];
+    }
+
+    for (int i=0; i < nums.length; i++) {
+      if (i < k) {
+        nums[i] = nums[i + shift];
+      } else {
+        nums[i] = store[i-k];
+      }
+    }
+
+  }
+
   public static void main (String[] args) {
     int [] arr = {1,2,3,4,5,6,7};
-    rotate(arr, 3);
+    rotate2(arr, 3);
     System.out.println(Arrays.toString(arr));
   }
 
